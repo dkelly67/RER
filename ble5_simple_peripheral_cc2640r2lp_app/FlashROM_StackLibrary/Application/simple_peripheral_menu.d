53061c218af0da155186278d3794b4a23742e923
Daniel Kelly <dkelly6793@gmail.com>
2017-11-12 17:24:52 -0600

    Added working project files

---
+++ ble5_simple_peripheral_cc2640r2lp_app/FlashROM_StackLibrary/Application/simple_peripheral_menu.d
@@ -0,0 +1,31 @@
+# FIXED
+
+Application/simple_peripheral_menu.obj: ../Application/simple_peripheral_menu.c
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/inc/bcomdef.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/osal/src/inc/comdef.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/../_common/cc26xx/_hal_types.h
+Application/simple_peripheral_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/inc/hal_defs.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/display/Display.h
+Application/simple_peripheral_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/common/cc26xx/menu/two_btn_menu.h
+Application/simple_peripheral_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+Application/simple_peripheral_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+Application/simple_peripheral_menu.obj: ../Application/simple_peripheral_menu.h
+Application/simple_peripheral_menu.obj: ../Application/simple_peripheral.h
+
+../Application/simple_peripheral_menu.c: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/inc/bcomdef.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/osal/src/inc/comdef.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/../_common/cc26xx/_hal_types.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/inc/hal_defs.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/display/Display.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/common/cc26xx/menu/two_btn_menu.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+../Application/simple_peripheral_menu.h: 
+../Application/simple_peripheral.h: 
---
+++ ble5_simple_peripheral_cc2640r2lp_app/FlashROM_StackLibrary/Application/two_btn_menu.d
@@ -0,0 +1,27 @@
+# FIXED
+
+Application/two_btn_menu.obj: ../Application/two_btn_menu.c
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/display/Display.h
+Application/two_btn_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+Application/two_btn_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/inc/bcomdef.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/osal/src/inc/comdef.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/../_common/cc26xx/_hal_types.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/inc/hal_defs.h
+Application/two_btn_menu.obj: ../Application/two_btn_menu.h
+Application/two_btn_menu.obj: C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+Application/two_btn_menu.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+
+../Application/two_btn_menu.c: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/display/Display.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/inc/bcomdef.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/osal/src/inc/comdef.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/../_common/cc26xx/_hal_types.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/inc/hal_defs.h: 
+../Application/two_btn_menu.h: 
+C:/ti/simplelink_cc2640r2_sdk_1_40_00_45/source/ti/ble5stack/hal/src/target/_common/hal_types.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
